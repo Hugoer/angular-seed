fb50bf51ca3a91dc305ea0b26b515e4f4e262dd5
hugoerdev <hugoerdev@gmail.com>
2020-01-08 12:21:16 +0100

    - Add component, form and route can deactivate guard

---
+++ src/app/core/guards/CanDeactivate/CanDeactivateGuard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+import { ComponentCanDeactivate } from './ComponentCanDeactivate';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CanDeactivateGuard implements CanDeactivate<ComponentCanDeactivate> {
+    constructor(
+        private translateService: TranslateService,
+    ) {
+
+    }
+
+    canDeactivate(component: ComponentCanDeactivate): boolean {
+
+        if (!component.canDeactivate()) {
+            const lostDataMessage = this.translateService.instant('global.lostdata');
+            if (confirm(lostDataMessage)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/app/core/guards/CanDeactivate/FormCanDeactivate.ts
@@ -0,0 +1,11 @@
+import { ComponentCanDeactivate } from './ComponentCanDeactivate';
+import { NgForm } from '@angular/forms';
+
+export abstract class FormCanDeactivate extends ComponentCanDeactivate {
+
+    abstract get form(): NgForm;
+
+    canDeactivate(): boolean {
+        return (this.form.submitted || !this.form.dirty);
+    }
+}
