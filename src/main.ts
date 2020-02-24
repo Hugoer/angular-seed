5e943ad6a245226f040708f45e4af5c530b2adbe
hugoerdev <hugoerdev@gmail.com>
2020-02-24 08:42:10 +0100

    - Stabilize version

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import '@angular/compiler';
-import { ɵrenderComponent } from '@angular/core';
-import { AppComponent } from './app/app.component';
+// import '@angular/compiler';
+// import { ɵrenderComponent } from '@angular/core';
+// import { AppComponent } from './app/app.component';
 
-ɵrenderComponent(AppComponent);
+// ɵrenderComponent(AppComponent);
+import 'hammerjs';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+    enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
