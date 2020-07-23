7d2d375c8811d76c6a902e50445edc152459d5f1
Hugoerdev <hugoerdev@gmail.com>
2020-07-23 23:07:41 +0200

    Force to detect user language or use environment property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { LocalStorageService } from 'ngx-webstorage';
+import { environment } from '@environment/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'seed';
-  constructor(private swUpdate: SwUpdate) {
+  constructor(
+    private swUpdate: SwUpdate,
+    private localStorage: LocalStorageService,
+
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.checkSW();
+    this.checkLanguage();
+  }
+
+  private checkSW() {
     if (this.swUpdate.isEnabled) {
-      swUpdate.checkForUpdate();
+      this.swUpdate.checkForUpdate();
       this.swUpdate.available.subscribe(() => {
         if (confirm(`Do you want to update ${this.title}?`)) {
           window.location.reload();
@@ -18,4 +33,12 @@ export class AppComponent {
       });
     }
   }
+
+  private checkLanguage() {
+    const language = this.localStorage.retrieve('userLanguage');
+    if (!language) {
+      this.localStorage.store('userLanguage', environment.defaultI18nLang);
+    }
+  }
+
 }
